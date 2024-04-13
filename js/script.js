faf7be63405bcf45d2f7315fa18e0e3292eeb5d6
Darius Gradinariu <gradinariudarius14@gmail.com>
2024-04-13 13:19:47 +0300

    show results 2

--- js/script.js
+++ js/script.js
@@ -58,35 +58,35 @@ async function displayPopularShows() {
     div.innerHTML = text;
     document.querySelector('#popular-shows').appendChild(div);
     
-    results.forEach(show => {
-        const div = document.createElement('div');
-        div.classList.add('card');
-        div.innerHTML = `
-          <a href="tv-details.html?id=${show.id}">
-            ${
-                show.poster_path ?
-                    `<img
-                        src="https://image.tmdb.org/t/p/w500${show.poster_path}"
-                        class="card-img-top"
-                        alt="${show.name}"
-                    />`
-                    :
-                    `<img
-                        src="../images/no-image.jpg"
-                        class="card-img-top"
-                        alt="${show.name}"
-                    />`
-            }
-          </a>
-          <div class="card-body">
-            <h5 class="card-title">${show.title}</h5>
-            <p class="card-text">
-              <small class="text-muted">Air Date: ${show.first_air_date}</small>
-            </p>
-          </div>`;
+    // results.forEach(show => {
+    //     const div = document.createElement('div');
+    //     div.classList.add('card');
+    //     div.innerHTML = `
+    //       <a href="tv-details.html?id=${show.id}">
+    //         ${
+    //             show.poster_path ?
+    //                 `<img
+    //                     src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+    //                     class="card-img-top"
+    //                     alt="${show.name}"
+    //                 />`
+    //                 :
+    //                 `<img
+    //                     src="../images/no-image.jpg"
+    //                     class="card-img-top"
+    //                     alt="${show.name}"
+    //                 />`
+    //         }
+    //       </a>
+    //       <div class="card-body">
+    //         <h5 class="card-title">${show.title}</h5>
+    //         <p class="card-text">
+    //           <small class="text-muted">Air Date: ${show.first_air_date}</small>
+    //         </p>
+    //       </div>`;
 
-          document.querySelector('#popular-shows').appendChild(div);
-    })
+    //       document.querySelector('#popular-shows').appendChild(div);
+    // })
 }
 
 // Display Movie Details
