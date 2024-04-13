971071cc0df1f19611063965489df06d028cee26
Darius Gradinariu <gradinariudarius14@gmail.com>
2024-04-13 14:11:37 +0300

    show results 4

--- js/script.js
+++ js/script.js
@@ -50,42 +50,37 @@ async function displayPopularMovies() {
 
 // Display 20 most popular tv shows
 async function displayPopularShows() {
-    const { results } = await fetchAPIData('tv/popular');
-
-    const div = document.createElement('div');
-    let text = 'abc';
-    div.innerHTML = text;
-    document.querySelector('#popular-shows').appendChild(div);
+    const { results } = await fetchAPIData('movie/popular');
     
-    // results.forEach(show => {
-    //     const div = document.createElement('div');
-    //     div.classList.add('card');
-    //     div.innerHTML = `
-    //       <a href="tv-details.html?id=${show.id}">
-    //         ${
-    //             show.poster_path ?
-    //                 `<img
-    //                     src="https://image.tmdb.org/t/p/w500${show.poster_path}"
-    //                     class="card-img-top"
-    //                     alt="${show.name}"
-    //                 />`
-    //                 :
-    //                 `<img
-    //                     src="../images/no-image.jpg"
-    //                     class="card-img-top"
-    //                     alt="${show.name}"
-    //                 />`
-    //         }
-    //       </a>
-    //       <div class="card-body">
-    //         <h5 class="card-title">${show.title}</h5>
-    //         <p class="card-text">
-    //           <small class="text-muted">Air Date: ${show.first_air_date}</small>
-    //         </p>
-    //       </div>`;
+    results.forEach(movie => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+            ${
+                movie.poster_path ?
+                    `<img
+                        src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                        class="card-img-top"
+                        alt="${movie.title}"
+                    />`
+                    :
+                    `<img
+                        src="../images/no-image.jpg"
+                        class="card-img-top"
+                        alt="${movie.title}"
+                    />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>`;
 
-    //       document.querySelector('#popular-shows').appendChild(div);
-    // })
+          document.querySelector('#popular-movies').appendChild(div);
+    });
 }
 
 // Display Movie Details
